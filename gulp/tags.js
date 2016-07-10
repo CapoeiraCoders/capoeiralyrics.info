53073b6e74528ecf841b981ced9bea32cb98239b
Vlad Tsepelev <reg.yeti@gmail.com>
2016-07-10 03:34:17 +0300

    fix tags rendering

--- gulp/tags.js
+++ gulp/tags.js
@@ -90,18 +90,7 @@ gulp.task('tags:build:pages', done => {
 			if(data.tags) {
 				data.tags.forEach(tag => {
 					tags[tag] = tags[tag] || [];
-
-					var song = {
-						slug: slugify(data.Name),
-						name: data.Name,
-						tags: data.tags.map(tag => {
-							return {
-								name: tag,
-								slug: slugify(tag)
-							}
-						})
-					};
-					tags[tag].push(song);
+					tags[tag].push(data);
 				})
 			}
 			return data;
@@ -114,7 +103,22 @@ gulp.task('tags:build:pages', done => {
 			for(var tag in tags){
 				var songs = tags[tag];
 				this.emit('data', new File({
-					contents:new Buffer(JSON.stringify({name:tag, slug:slugify(tag), songs:songs})),
+					contents:new Buffer(JSON.stringify({
+						name: tag,
+						slug: slugify(tag),
+						songs: songs.map(song => {
+							return {
+								name: song.Name,
+								slug: slugify(song.Name),
+								tags: song.tags.map(tag => {
+									return {
+										name: tag,
+										slug: slugify(tag)
+									}
+								})
+							}
+						})
+					})),
 					path: './'+slugify(tag)+'.json'
 
 				}));
@@ -124,9 +128,7 @@ gulp.task('tags:build:pages', done => {
 	))
 	.pipe(middlewares.mustache(BASE_TEMPLATE_PATH)) // render via mustache
 	.pipe(rename({extname:'.html'}))
-	// .pipe(es.through(data => {
-		// console.log(data)
-	// }))
+	
 	.pipe(gulp.dest('./public/tags/'));
 });
 
